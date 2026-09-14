6dd6506c4e65cae5c7ae546f28c2f5639e653869
David Soto Garcia <dsoto-ga@c3r13s5.42madrid.com>
2026-09-14 17:15:23 +0200

    feat: create base header

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,76 @@
 /*   By: dsoto-ga <dsoto-ga@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/23 12:56:39 by dsoto-ga          #+#    #+#             */
-/*   Updated: 2026/08/23 12:56:40 by dsoto-ga         ###   ########.fr       */
+/*   Updated: 2026/09/14 16:58:26 by dsoto-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+
+typedef struct s_node
+{
+	int				content;
+	struct s_node	*next;
+	struct s_node	*prev;
+	int				index;
+}	t_node;
+
+typedef struct s_stack
+{
+	struct s_node	*top;
+	struct s_node	*bottom;
+	int				size;
+}	t_stack;
+
+typedef struct s_counters
+{
+	int	pa;
+	int	pb;
+	int	sa;
+	int	sb;
+	int	ss;
+	int	ra;
+	int	rb;
+	int	rr;
+	int	rra;
+	int	rrb;
+	int	rrr;
+}	t_counters;
+
+typedef enum e_strategy
+{
+	STRAT_ADAPTIVE,
+	STRAT_SIMPLE,
+	STRAT_MEDIUM,
+	STRAT_COMPLEX,
+}	t_strategy;
+
+typedef struct s_context
+{
+	t_stack		a;
+	t_stack		b;
+	t_counters	ops;
+	t_strategy	strategy;
+	double		disorder;
+	int			bench_mode;	
+}	t_context;
+
+/* parsing */
+int	parse_args(int argc, char *argv[], t_context *ctx);
+
+/* stack*/
+
+/* operations */
+
+/* algorithms*/
+
+/* bench */
+
+/* utils */
+
+#endif
